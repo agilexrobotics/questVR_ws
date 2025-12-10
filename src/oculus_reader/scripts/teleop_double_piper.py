4daf04c625cfb515bd26fe5ce30e52bb84c8a4a9
RoboPPN <2220147287@qq.com>
2025-12-10 15:29:28 +0800

    提高遥操帧率

--- src/oculus_reader/scripts/teleop_double_piper.py
+++ src/oculus_reader/scripts/teleop_double_piper.py
@@ -4,7 +4,7 @@ import tf2_ros
 import rospkg
 import geometry_msgs.msg
 from tf.transformations import quaternion_from_matrix
-from tf.transformations import quaternion_from_euler, quaternion_from_matrix
+from tf.transformations import quaternion_from_euler, quaternion_from_matrix, euler_from_quaternion
 
 import casadi
 import meshcat.geometry as mg
@@ -19,6 +19,8 @@ import math
 
 from tools import MATHTOOLS
 from piper_control import PIPER
+
+from geometry_msgs.msg import PoseStamped
 
 def matrix_to_xyzrpy(matrix):
     x = matrix[0, 3]
@@ -76,7 +78,12 @@ class Arm_IK:
         package_path = rospack.get_path('piper_description') 
 
         urdf_path = os.path.join(package_path, 'urdf', 'piper_description.urdf')
-        self.robot = pin.RobotWrapper.BuildFromURDF(urdf_path)
+        
+        self.robot = pin.RobotWrapper.BuildFromURDF(
+            urdf_path,
+            package_dirs=package_path
+        )
+        
         self.mixed_jointsToLockIDs = ["joint7",
                                       "joint8"
                                       ]
@@ -243,7 +250,7 @@ class Arm_IK:
 
             is_collision = self.check_self_collision(sol_q, gripper)
             dist = self.get_dist(sol_q, target_pose[:3, 3])
-            print("dist:", dist)
+            # print("dist:", dist)
             return sol_q, tau_ff, not is_collision
 
         except Exception as e:
@@ -259,7 +266,7 @@ class Arm_IK:
         return collision
 
     def get_dist(self, q, xyz):
-        print("q:", q)
+        # print("q:", q)
         pin.forwardKinematics(self.reduced_robot.model, self.reduced_robot.data, np.concatenate([q], axis=0))
         dist = math.sqrt(pow((xyz[0] - self.reduced_robot.data.oMi[6].translation[0]), 2) + pow((xyz[1] - self.reduced_robot.data.oMi[6].translation[1]), 2) + pow((xyz[2] - self.reduced_robot.data.oMi[6].translation[2]), 2))
         return dist
@@ -282,47 +289,23 @@ class VR:
         self.L_inverse_solution = Arm_IK()
         self.piper_control.left_init_pose()
         self.piper_control.right_init_pose()
-
-    def adjustment_matrix(self,transform):
-        if transform.shape != (4, 4):
-            raise ValueError("Input transform must be a 4x4 numpy array.")
         
-        adj_mat = np.array([
-            [0,0,-1,0],
-            [-1,0,0,0],
-            [0,1,0,0],
-            [0,0,0,1]
-        ])
+        # 这里可选为 WIFI连接 或 USB连接
+        # oculus_reader = OculusReader(ip_address='10.12.11.14')    #  WIFI连接
+        self.oculus_reader = OculusReader()                         #  USB连接
         
-        r_adj = self.tools.xyzrpy2Mat(0,0,0,   -np.pi , 0, -np.pi/2)
+        # 延时0.5秒，确保 OculusReader 初始化完成   
+        import time
+        time.sleep(0.5)
         
-        transform = adj_mat @ transform  
+        # 夹爪坐标系到到基坐标系的变换
+        self.base_RR = [0.19, 0.0, 0.2, 0, 0, 0]
+        self.base_LL = [0.19, 0.0, 0.2, 0, 0, 0]
+
+        # 订阅回调
+        rospy.Subscriber('/right_handle_pose', PoseStamped, self.right_handle_pose_callback, queue_size=1)
+        rospy.Subscriber('/left_handle_pose', PoseStamped, self.left_handle_pose_callback, queue_size=1)
         
-        transform = np.dot(transform, r_adj)  
-        
-        return transform
-
-    def publish_transform(self,transform, name):
-        translation = transform[:3, 3]
-
-        br = tf2_ros.TransformBroadcaster()
-        t = geometry_msgs.msg.TransformStamped()
-
-        t.header.stamp = rospy.Time.now()
-        t.header.frame_id = 'vr_device'
-        t.child_frame_id = name
-        t.transform.translation.x = translation[0]
-        t.transform.translation.y = translation[1]
-        t.transform.translation.z = translation[2]
-
-        quat = quaternion_from_matrix(transform)
-        t.transform.rotation.x = quat[0]
-        t.transform.rotation.y = quat[1]
-        t.transform.rotation.z = quat[2]
-        t.transform.rotation.w = quat[3]
-
-        br.sendTransform(t)
-    
     def R_get_ik_solution(self, x,y,z,roll,pitch,yaw,gripper,b):
         
         q = quaternion_from_euler(roll, pitch, yaw)
@@ -351,61 +334,55 @@ class VR:
         if  b :
             self.piper_control.left_joint_control_piper(sol_q[0],sol_q[1],sol_q[2],sol_q[3],sol_q[4],sol_q[5],gripper)
         else :
-            print("collision!!!")   
-    def Run(self):
-        # 这里可选为 WIFI连接 或 USB连接
-        # oculus_reader = OculusReader(ip_address='10.12.11.14')    #  WIFI连接
-        oculus_reader = OculusReader()                              #  USB连接
-        
+            print("collision!!!") 
 
-        rate = rospy.Rate(50)
+    def right_handle_pose_callback(self, msg):
+        # print(msg.pose.position.x, msg.pose.position.y, msg.pose.position.z)
+        x = msg.pose.position.x
+        y = msg.pose.position.y
+        z = msg.pose.position.z
+        (roll, pitch, yaw) = euler_from_quaternion([msg.pose.orientation.x, msg.pose.orientation.y, msg.pose.orientation.z, msg.pose.orientation.w])
         
-        # 夹爪坐标系到到基坐标系的变换
-        base_RR = [0.19, 0.0, 0.2, 0, 0, 0]
-        base_LL = [0.19, 0.0, 0.2, 0, 0, 0]
-        
-        while not rospy.is_shutdown():
-            rate.sleep()
-            transformations, buttons = oculus_reader.get_transformations_and_buttons()
-            if 'r' not in transformations :
-                continue
-            
-            transformations['r'] = self.adjustment_matrix(transformations['r'])
-            transformations['l'] = self.adjustment_matrix(transformations['l'])
-            
-            right_controller_pose = transformations['r']
-            
-            left_controller_pose = transformations['l']
-            
-            self.publish_transform(right_controller_pose, 'right_hand')
-            self.publish_transform(left_controller_pose, 'left_hand')
-            
-            RR = self.tools.matrix2Pose(transformations['r'])
-            LL = self.tools.matrix2Pose(transformations['l'])
-                
-            if buttons['A'] == True :
-                # 按下A键后，机械臂回到初始点位并且记录 右 坐标原点
-                self.piper_control.right_init_pose()
-                base_RR = self.tools.matrix2Pose(transformations['r'])
-                
-            if buttons['X'] == True  :
-                # 按下X键后，机械臂回到初始点位并且记录 左 坐标原点
-                self.piper_control.left_init_pose()
-                base_LL = self.tools.matrix2Pose(transformations['l'])
+        _, buttons = self.oculus_reader.get_transformations_and_buttons()
 
-            RR_ = calc_pose_incre(base_RR,RR)
-            LL_ = calc_pose_incre(base_LL,LL)
+        RR = [x,y,z,roll,pitch,yaw]
             
+        if buttons['A'] == True :
+            # 按下A键后，机械臂回到初始点位并且记录 右 坐标原点
+            self.piper_control.init_pose()
+            self.base_RR = [x,y,z,roll,pitch,yaw]
+                    
+        RR_ = calc_pose_incre(self.base_RR,RR)
+        
+        r_gripper_value = buttons['rightTrig'][0] * 0.07 
+        # 按下B键后，开始遥操作
+        self.R_get_ik_solution(RR_[0],RR_[1],RR_[2],RR_[3],RR_[4],RR_[5],r_gripper_value,buttons['B'])
+
+
+    def left_handle_pose_callback(self, msg):
+        # print(msg.pose.position.x, msg.pose.position.y, msg.pose.position.z)
+        x = msg.pose.position.x
+        y = msg.pose.position.y
+        z = msg.pose.position.z
+        (roll, pitch, yaw) = euler_from_quaternion([msg.pose.orientation.x, msg.pose.orientation.y, msg.pose.orientation.z, msg.pose.orientation.w])
+        
+        _, buttons = self.oculus_reader.get_transformations_and_buttons()
+
+        LL = [x,y,z,roll,pitch,yaw]
             
-            r_gripper_value = buttons['rightTrig'][0] * 0.07  #右夹爪值
-            l_gripper_value = buttons['leftTrig'][0] * 0.07   #左夹爪值
-            
-            # 按下B、Y键开始遥操作
-            self.R_get_ik_solution(RR_[0],RR_[1],RR_[2],RR_[3],RR_[4],RR_[5],r_gripper_value,buttons['B'])
-            self.L_get_ik_solution(LL_[0],LL_[1],LL_[2],LL_[3],LL_[4],LL_[5],l_gripper_value,buttons['Y']) 
+        if buttons['X'] == True :
+            # 按下X键后，机械臂回到初始点位并且记录 左 坐标原点
+            self.piper_control.init_pose()
+            self.base_LL = [x,y,z,roll,pitch,yaw]
+                    
+        RR_ = calc_pose_incre(self.base_LL,LL)
+        
+        r_gripper_value = buttons['leftTrig'][0] * 0.07 
+        # 按下B键后，开始遥操作
+        self.L_get_ik_solution(RR_[0],RR_[1],RR_[2],RR_[3],RR_[4],RR_[5],r_gripper_value,buttons['Y'])
 
 if __name__ == '__main__':
-    rospy.init_node('oculus_reader')
+    rospy.init_node('teleop_double_piper_node')
     vr = VR()
-    vr.Run()
+    rospy.spin()
     
--- src/oculus_reader/scripts/teleop_single_piper.py
+++ src/oculus_reader/scripts/teleop_single_piper.py
@@ -4,7 +4,7 @@ import tf2_ros
 import rospkg
 import geometry_msgs.msg
 from tf.transformations import quaternion_from_matrix
-from tf.transformations import quaternion_from_euler, quaternion_from_matrix
+from tf.transformations import quaternion_from_euler, quaternion_from_matrix, euler_from_quaternion
 
 import casadi
 import meshcat.geometry as mg
@@ -19,6 +19,8 @@ import math
 
 from tools import MATHTOOLS
 from piper_control import PIPER
+
+from geometry_msgs.msg import PoseStamped
 
 def matrix_to_xyzrpy(matrix):
     x = matrix[0, 3]
@@ -77,7 +79,10 @@ class Arm_IK:
 
         urdf_path = os.path.join(package_path, 'urdf', 'piper_description.urdf')
         
-        self.robot = pin.RobotWrapper.BuildFromURDF(urdf_path)
+        self.robot = pin.RobotWrapper.BuildFromURDF(
+            urdf_path,
+            package_dirs=package_path
+        )
 
         self.mixed_jointsToLockIDs = ["joint7",
                                       "joint8"
@@ -281,54 +286,23 @@ class Arm_IK:
 
 class VR:
     def __init__(self):
-        rospy.init_node('oculus_reader')
         self.piper_control = PIPER()
         self.tools = MATHTOOLS()
         self.inverse_solution = Arm_IK()
         self.piper_control.init_pose()
-
-    def adjustment_matrix(self,transform):
-
-        if transform.shape != (4, 4):
-            raise ValueError("Input transform must be a 4x4 numpy array.")
         
-        adj_mat = np.array([
-            [0,0,-1,0],
-            [-1,0,0,0],
-            [0,1,0,0],
-            [0,0,0,1]
-        ])
+        # 这里可选为 WIFI连接 或 USB连接
+        # oculus_reader = OculusReader(ip_address='10.12.11.14')    #  WIFI连接
+        self.oculus_reader = OculusReader()                         #  USB连接
         
-        r_adj = self.tools.xyzrpy2Mat(0,0,0,   -np.pi , 0, -np.pi/2)
+        # 延时0.5秒，确保 OculusReader 初始化完成   
+        import time
+        time.sleep(0.5)
 
-        transform = adj_mat @ transform  
+        self.base_RR = [0.19, 0.0, 0.2, 0, 0, 0]
+        # 订阅回调
+        rospy.Subscriber('/right_handle_pose', PoseStamped, self.handle_pose_callback, queue_size=1)
         
-        transform = np.dot(transform, r_adj)  
-        
-        return transform
-
-    def publish_transform(self,transform, name):
-        translation = transform[:3, 3]
-
-        br = tf2_ros.TransformBroadcaster()
-        t = geometry_msgs.msg.TransformStamped()
-
-        t.header.stamp = rospy.Time.now()
-        t.header.frame_id = 'vr_device'
-        t.child_frame_id = name
-        t.transform.translation.x = translation[0]
-        t.transform.translation.y = translation[1]
-        t.transform.translation.z = translation[2]
-
-        quat = quaternion_from_matrix(transform)
-        t.transform.rotation.x = quat[0]
-        t.transform.rotation.y = quat[1]
-        t.transform.rotation.z = quat[2]
-        t.transform.rotation.w = quat[3]
-
-        br.sendTransform(t)
-    
-    
     def get_ik_solution(self, x,y,z,roll,pitch,yaw,gripper,b):
         
         q = quaternion_from_euler(roll, pitch, yaw)
@@ -344,44 +318,32 @@ class VR:
             print("ctrolling!!!")
         # else :
         #     print("collision!!!")
-            
-    def Run(self):
-        # 这里可选为 WIFI连接 或 USB连接
-        # oculus_reader = OculusReader(ip_address='10.12.11.14')    #  WIFI连接
-        oculus_reader = OculusReader()                              #  USB连接
-        
 
-        rate = rospy.Rate(50)
+    def handle_pose_callback(self, msg):
+        # print(msg.pose.position.x, msg.pose.position.y, msg.pose.position.z)
+        self.x = msg.pose.position.x
+        self.y = msg.pose.position.y
+        self.z = msg.pose.position.z
+        (self.roll, self.pitch, self.yaw) = euler_from_quaternion([msg.pose.orientation.x, msg.pose.orientation.y, msg.pose.orientation.z, msg.pose.orientation.w])
         
-        # 夹爪坐标系到到基坐标系的变换
-        base_RR = [0.19, 0.0, 0.2, 0, 0, 0]
+        _, buttons = self.oculus_reader.get_transformations_and_buttons()
+
+        RR = [self.x,self.y,self.z,self.roll,self.pitch,self.yaw]
+            
+        if buttons['A'] == True :
+            # 按下A键后，机械臂回到初始点位并且记录 右 坐标原点
+            self.piper_control.init_pose()
+            self.base_RR = [self.x,self.y,self.z,self.roll,self.pitch,self.yaw]
+                    
+        RR_ = calc_pose_incre(self.base_RR,RR)
         
-        while not rospy.is_shutdown():
-            rate.sleep()
-            transformations, buttons = oculus_reader.get_transformations_and_buttons()
-            if 'r' not in transformations :
-                continue
+        r_gripper_value = buttons['rightTrig'][0] * 0.07 
+        # 按下B键后，开始遥操作
+        self.get_ik_solution(RR_[0],RR_[1],RR_[2],RR_[3],RR_[4],RR_[5],r_gripper_value,buttons['B'])
             
-            transformations['r'] = self.adjustment_matrix(transformations['r'])
-            
-            right_controller_pose = transformations['r']
-            
-            self.publish_transform(right_controller_pose, 'right_hand')
-            
-            RR = self.tools.matrix2Pose(transformations['r'])
-                
-            if buttons['A'] == True :
-                # 按下A键后，机械臂回到初始点位并且记录 右 坐标原点
-                self.piper_control.init_pose()
-                base_RR = self.tools.matrix2Pose(transformations['r'])
-                        
-            RR_ = calc_pose_incre(base_RR,RR)
-            
-            r_gripper_value = buttons['rightTrig'][0] * 0.07 
-            # 按下B键后，开始遥操作
-            self.get_ik_solution(RR_[0],RR_[1],RR_[2],RR_[3],RR_[4],RR_[5],r_gripper_value,buttons['B'])
 
 if __name__ == '__main__':
+    rospy.init_node('teleop_single_piper_node')
     vr = VR()
-    vr.Run()
+    rospy.spin()
     
